e989efe2d498f8a5491c0b0f3bc7fd3084238896
Daniel Silva <dfsilva.dxp@gmail.com>
2022-10-25 19:12:15 -0300

    test: add base layout test

---
+++ src/template/Base/Base.test.tsx
@@ -0,0 +1,28 @@
+import { screen } from "@testing-library/react";
+
+import { renderWithTheme } from "utils/test/helpers";
+
+import { Base } from "template";
+
+jest.mock("components/Header", () => {
+  return {
+    __esModule: true,
+    default: function Mock() {
+      return <header />;
+    }
+  };
+});
+
+describe("<Base/>", () => {
+  it("should render header and content", () => {
+    renderWithTheme(<Base />);
+
+    const header = screen.getByRole("banner");
+
+    expect(header).toBeInTheDocument();
+    expect(header.nextElementSibling).toHaveStyle({
+      "max-width": "71.25rem",
+      padding: "2.5rem 1rem"
+    });
+  });
+});
