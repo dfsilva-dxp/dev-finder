082a2bea72dabb9d78489e3ac8891750c6795a5d
Daniel Silva <dfsilva.dxp@gmail.com>
2022-10-26 15:16:53 -0300

    test: add test to called fn when button is clicked

--- src/components/Button/Button.test.tsx
+++ src/components/Button/Button.test.tsx
@@ -1,5 +1,7 @@
 import { screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import { SunDim } from "phosphor-react";
+import { act } from "react-dom/test-utils";
 import { renderWithTheme } from "utils/test/helpers";
 
 import Button from ".";
@@ -32,5 +34,31 @@ describe("<Button/>", () => {
     expect(screen.getByTestId("icon")).toBeInTheDocument();
 
     expect(container.parentElement).toMatchSnapshot();
+  });
+
+  it("should be accessible with tab", () => {
+    renderWithTheme(<Button>Theme</Button>);
+
+    expect(document.body).toHaveFocus();
+
+    userEvent.tab();
+
+    expect(screen.getByRole("button", { name: /theme/i })).toHaveFocus();
+  });
+
+  it("should be called the fn() when button is cliecked", async () => {
+    const toggleIcon = jest.fn();
+
+    act(() => {
+      renderWithTheme(<Button onClick={toggleIcon}>Theme</Button>);
+    });
+
+    const button = screen.getByRole("button", { name: /theme/i });
+
+    act(() => {
+      button.dispatchEvent(new Event("click", { bubbles: true }));
+    });
+
+    expect(toggleIcon).toHaveBeenCalledTimes(1);
   });
 });
