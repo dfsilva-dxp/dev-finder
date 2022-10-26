1c02d54a4c9758610afea220b2b48d451ac59378
Daniel Silva <dfsilva.dxp@gmail.com>
2022-10-26 16:50:55 -0300

    test: add search form test

---
+++ src/components/SearchForm/SearchForm.test.tsx
@@ -0,0 +1,29 @@
+import { screen, waitFor } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+
+import { renderWithTheme } from "utils/test/helpers";
+
+import SearchForm from ".";
+
+describe("<SearchForm", () => {
+  it("should renders with placeholder", () => {
+    renderWithTheme(<SearchForm />);
+
+    expect(
+      screen.getByPlaceholderText(/search github username.../i)
+    ).toBeInTheDocument();
+  });
+
+  it("should changes its value when typing", async () => {
+    renderWithTheme(<SearchForm />);
+
+    const input = screen.getByPlaceholderText(/search github username.../i);
+    const text = "This is my new text";
+
+    userEvent.type(input, text);
+
+    await waitFor(() => {
+      expect(input).toHaveValue(text);
+    });
+  });
+});
