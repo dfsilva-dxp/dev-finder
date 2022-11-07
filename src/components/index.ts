7ee44c3a575e29aae872f7ac4d5857e9d8f308e8
Daniel Silva <dfsilva.dxp@gmail.com>
2022-11-07 19:26:55 -0300

    feat: add export default

--- src/components/index.ts
+++ src/components/index.ts
@@ -2,4 +2,6 @@ export { default as Avatar } from "./Avatar";
 export { default as Button } from "./Button";
 export { default as Card } from "./Card";
 export { default as Header } from "./Header";
+export { default as Loading } from "./Loading";
+export { default as Message } from "./Message";
 export { default as SearchForm } from "./SearchForm";
