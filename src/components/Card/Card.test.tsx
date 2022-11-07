aa0b3fd7831258b94ce6f59e11496873887b14be
Daniel Silva <dfsilva.dxp@gmail.com>
2022-11-07 19:05:23 -0300

    test: add card test

--- src/components/Card/Card.test.tsx
+++ src/components/Card/Card.test.tsx
@@ -3,26 +3,35 @@ import { renderWithTheme } from "utils/test/helpers";
 
 import Card from ".";
 
+import userMock from "./mock";
+
 describe("<Card/>", () => {
   it("should render currectly", () => {
-    const { container } = renderWithTheme(<Card />);
+    const { container } = renderWithTheme(<Card user={userMock} />);
 
     expect(
-      screen.getByRole("img", { name: /user avatar/i })
+      screen.getByRole("img", { name: /User Avatar/i })
     ).toBeInTheDocument();
 
     expect(
-      screen.getByRole("heading", { name: /daniel silva/i })
+      screen.getByRole("heading", { name: userMock.name })
     ).toBeInTheDocument();
 
-    expect(screen.getByRole("link", { name: "dfsilva-dxp" })).toHaveAttribute(
+    expect(screen.getByRole("link", { name: userMock.login })).toHaveAttribute(
       "href",
-      "/link"
+      userMock.html_url
     );
 
-    expect(
-      screen.getByText(/Lorem ipsum dolor sit amet, consectetur adipis/i)
-    ).toBeInTheDocument();
+    expect(screen.getByText(userMock.bio)).toBeInTheDocument();
+
+    expect(screen.getByText(/repos/i)).toBeInTheDocument();
+    expect(screen.getByText(userMock.public_repos)).toBeInTheDocument();
+
+    expect(screen.getByText(/followers/i)).toBeInTheDocument();
+    expect(screen.getByText(userMock.followers)).toBeInTheDocument();
+
+    expect(screen.getByText(/following/i)).toBeInTheDocument();
+    expect(screen.getByText(userMock.following)).toBeInTheDocument();
 
     expect(container).toMatchSnapshot();
   });
