74aea2a7b7df14461de3eb0b62266870c4713190
Daniel Silva <dfsilva.dxp@gmail.com>
2022-11-08 09:30:43 -0300

    test: add Base component test

--- src/template/Base/Base.test.tsx
+++ src/template/Base/Base.test.tsx
@@ -4,6 +4,15 @@ import { renderWithTheme } from "utils/test/helpers";
 
 import { Base } from "template";
 
+jest.mock("components/SearchForm", () => {
+  return {
+    __esModule: true,
+    default: function Mock() {
+      return <form data-testid="Mock Form"></form>;
+    }
+  };
+});
+
 describe("<Base/>", () => {
   it("should render header and content", () => {
     const onToggleTheme = jest.fn();
@@ -16,7 +25,7 @@ describe("<Base/>", () => {
     expect(header).toBeInTheDocument();
     expect(header.parentElement).toHaveStyle({
       width: "100%",
-      "max-width": "61.9375rem",
+      "max-width": "45rem",
       height: "100%",
       margin: "0 auto",
       padding: "2.5rem 1rem"
