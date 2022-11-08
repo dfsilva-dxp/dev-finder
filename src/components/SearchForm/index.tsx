c071c69a28876825c721a93992890c2ae571f28b
Daniel Silva <dfsilva.dxp@gmail.com>
2022-11-08 09:29:07 -0300

    refactor: refatoring search form

--- src/components/SearchForm/index.tsx
+++ src/components/SearchForm/index.tsx
@@ -22,7 +22,7 @@ export default function SearchForm() {
   };
 
   return (
-    <Form onSubmit={handleSearchUser}>
+    <Form onSubmit={handleSearchUser} aria-label="Search a GitHub username">
       <MagnifyingGlass size={20} />
       <input
         type="search"
