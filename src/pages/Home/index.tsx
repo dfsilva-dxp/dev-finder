8884b5577d8726159941b2edb76e0beea4404024
Daniel Silva <dfsilva.dxp@gmail.com>
2022-11-16 14:08:30 -0300

    refactor: refatoring home page

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,12 +1,14 @@
-import { Card, Loading, Message, SearchForm } from "components";
+import { Card, Head, Loading, Message, SearchBar } from "components";
 
 import useGithubUser from "hooks/useGithubUser";
 
 export default function Home() {
   const { storedUser, loading, message } = useGithubUser();
+
   return (
     <>
-      <SearchForm />
+      <Head title="devFinder | Search" description="Search a github username" />
+      <SearchBar />
       {loading ? (
         <Loading />
       ) : storedUser ? (
